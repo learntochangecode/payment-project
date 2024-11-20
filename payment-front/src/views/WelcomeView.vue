1c063ca6a9d9f3dd40b7676372b77a2bd5de7b17
William Zeyar <dulkred@gmail.com>
2024-11-20 11:56:03 +0800

    1.初始化merchant商户端

--- payment-front/src/views/WelcomeView.vue
+++ payment-front/src/views/WelcomeView.vue
@@ -6,8 +6,6 @@
         </div>
         <div class="welcome-title">
             <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
-            <div style="margin-top: 10px">在这里你可以学习如何使用Java，如何搭建网站，并且与Java之父密切交流。</div>
-            <div style="margin-top: 5px">在这里你可以同性交友，因为都是男的，没有学Java的女生。</div>
         </div>
         <div class="right-card">
             <router-view v-slot="{ Component }">
---
+++ payment-merchant/src/main/java/com/example/paymentmerchant/PaymentMerchantApplication.java
@@ -0,0 +1,13 @@
+package com.example.paymentmerchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentMerchantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMerchantApplication.class, args);
+    }
+
+}
